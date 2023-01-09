d0590206d26cdb47caddd0ead0c463e6963e6edf
lee yun seok <92229562+leeeeys@users.noreply.github.com>
2023-01-09 18:27:49 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# portfolio-index
+# musical-octo-doodle
---
+++ index.js
@@ -0,0 +1,45 @@
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+     //console.log(scroll);
+    if (scroll >600) {
+        //console.log('a');
+        $("header").addClass("active");
+    } else {
+        //console.log('a');
+        $("header").removeClass("active");
+    }
+});
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+     //console.log(scroll);
+    if (scroll > 600) {
+        //console.log('a');
+        $(".head-inner").addClass("active2");
+    } else {
+        //console.log('a');
+        $(".head-inner").removeClass("active2");
+    }
+});
+
+        // scroll-animation
+        const saTriggerMargin = 300;
+      const saElementList = document.querySelectorAll('.sa');
+
+      const saFunc = function() {
+        for (const element of saElementList) {
+          if (!element.classList.contains('show')) {
+            if (window.innerHeight > element.getBoundingClientRect().top + saTriggerMargin) {
+              element.classList.add('show');
+            }
+          }
+        }
+      }
+      window.addEventListener('load', saFunc);
+      window.addEventListener('scroll', saFunc);
+    // 푸터 미디어쿼리 메뉴
+    $(".media-gnb ul.media-sub").hide();
+    $(".media-gnb ul.media-menu li").on('click', function () {
+        $("ul", this).slideToggle("fast");
+        $(".accordion", this).toggleClass("active");
+    })
